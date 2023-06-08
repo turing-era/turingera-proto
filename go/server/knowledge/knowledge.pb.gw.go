23d2f98fe2d361e842a74b9d9388373b9fb4e378
bennysong <bennysong@tencent.com>
2023-06-08 13:27:21 +0800

    feat: proto

--- go/server/knowledge/knowledge.pb.gw.go
+++ go/server/knowledge/knowledge.pb.gw.go
@@ -167,40 +167,6 @@ func local_request_KnowledgeSvr_LoadKnowledge_0(ctx context.Context, marshaler r
 
 }
 
-func request_KnowledgeSvr_SearchKnowledge_0(ctx context.Context, marshaler runtime.Marshaler, client KnowledgeSvrClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq SearchKnowledgeReq
-	var metadata runtime.ServerMetadata
-
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := client.SearchKnowledge(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
-func local_request_KnowledgeSvr_SearchKnowledge_0(ctx context.Context, marshaler runtime.Marshaler, server KnowledgeSvrServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq SearchKnowledgeReq
-	var metadata runtime.ServerMetadata
-
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := server.SearchKnowledge(ctx, &protoReq)
-	return msg, metadata, err
-
-}
-
 // RegisterKnowledgeSvrHandlerServer registers the http handlers for service KnowledgeSvr to "mux".
 // UnaryRPC     :call KnowledgeSvrServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -304,31 +270,6 @@ func RegisterKnowledgeSvrHandlerServer(ctx context.Context, mux *runtime.ServeMu
 		}
 
 		forward_KnowledgeSvr_LoadKnowledge_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
-	mux.Handle("POST", pattern_KnowledgeSvr_SearchKnowledge_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		var stream runtime.ServerTransportStream
-		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		var err error
-		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/turingera.server.knowledge.KnowledgeSvr/SearchKnowledge", runtime.WithHTTPPathPattern("/knowledge/SearchKnowledge"))
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := local_request_KnowledgeSvr_SearchKnowledge_0(annotatedContext, inboundMarshaler, server, req, pathParams)
-		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
-		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
-		if err != nil {
-			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_KnowledgeSvr_SearchKnowledge_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -461,28 +402,6 @@ func RegisterKnowledgeSvrHandlerClient(ctx context.Context, mux *runtime.ServeMu
 
 	})
 
-	mux.Handle("POST", pattern_KnowledgeSvr_SearchKnowledge_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		var err error
-		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/turingera.server.knowledge.KnowledgeSvr/SearchKnowledge", runtime.WithHTTPPathPattern("/knowledge/SearchKnowledge"))
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_KnowledgeSvr_SearchKnowledge_0(annotatedContext, inboundMarshaler, client, req, pathParams)
-		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
-		if err != nil {
-			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_KnowledgeSvr_SearchKnowledge_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
 	return nil
 }
 
@@ -494,8 +413,6 @@ var (
 	pattern_KnowledgeSvr_RemKnowledge_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"knowledge", "RemKnowledge"}, ""))
 
 	pattern_KnowledgeSvr_LoadKnowledge_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"knowledge", "LoadKnowledge"}, ""))
-
-	pattern_KnowledgeSvr_SearchKnowledge_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"knowledge", "SearchKnowledge"}, ""))
 )
 
 var (
@@ -506,6 +423,4 @@ var (
 	forward_KnowledgeSvr_RemKnowledge_0 = runtime.ForwardResponseMessage
 
 	forward_KnowledgeSvr_LoadKnowledge_0 = runtime.ForwardResponseMessage
-
-	forward_KnowledgeSvr_SearchKnowledge_0 = runtime.ForwardResponseMessage
 )
